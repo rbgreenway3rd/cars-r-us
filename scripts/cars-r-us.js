362466f366d158eb1e7eba4aeb03d49e07619dbf
rbgreenway3rd <3rdgreenway@gmail.com>
2021-05-04 13:05:09 -0500

    added modules for each car option, began creating dynamic html

---
+++ scripts/cars-r-us.js
@@ -0,0 +1,10 @@
+export const cars_r_us = () => {
+    return /*html*/`
+        <h1>Cars R Us</h1>
+        
+        <article class="options">
+            <section class="options__paints">
+                <h2>Colors</h2>
+                ${Colors()}
+            </section>`
+}
